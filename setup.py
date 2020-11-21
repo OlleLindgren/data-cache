25258784bc35220ecf449bfb8c2cce218a6f4703
Olle Lindgren <lindgrenolle@live.se>
2020-11-21 18:48:08 +0100

    Create setup.py, __init__.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="file-caching-olleli", # Replace with your own username
+    version="0.0.1",
+    author="Olle Lindgren",
+    author_email="lindgrenolle@live.se",
+    description="A package for caching files locally",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OlleLindgren/file-caching",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
