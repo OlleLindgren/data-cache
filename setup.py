9b055b0094ea2c99926fbc5319d601c816a8fa84
Olle Lindgren <lindgrenolle@live.se>
2020-11-22 10:57:39 +0100

    Rename package to file-caching

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="file_caching", # Replace with your own username
+    name="file-caching", # Replace with your own username
     version="0.0.1",
     author="Olle Lindgren",
     author_email="lindgrenolle@live.se",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/OlleLindgren/file-caching",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'pandas',
+          'pyarrow',
+          'termcolor'
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
