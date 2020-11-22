f4a792cab3b6043a032a0bea4ebc843b3be0e0d8
Olle Lindgren <lindgrenolle@live.se>
2020-11-22 11:15:37 +0100

    Made var names more readable

--- dfcache/caching.py
+++ dfcache/caching.py
@@ -3,71 +3,79 @@ import os
 import time
 from termcolor import colored
 
-def read_cache(_d, _verbose=False):
+def read_cache(filename, verbose=False):
     def _print(msg, opt_dir=None):
         _col = 'green'
         _dir_col = 'blue'
-        if _verbose and opt_dir is None:
+        if verbose and opt_dir is None:
             print(colored(msg, _col))
-        elif _verbose:
+        elif verbose:
             print(colored(msg, _col) + ' ' + colored(opt_dir, _dir_col))
     
     t0 = time.time()
-    _print('* Read request for', _d)
+    _print('* Read request for', filename)
 
-    _filedir, _ext = os.path.splitext(_d)
+    _filedir, _ext = os.path.splitext(filename)
+
+    # If file already in feather format, return file.
+    if _ext == '.feather' and os.path.isfile(filename):
+        result = pd.read_feather(_ext)
+        t1 = time.time()
+        _print(f'  * {(t1-t0)*1000:.3f} ms elapsed')
+        return result
+
     _folder, _filename = os.path.split(_filedir)
     assert _folder[:4] == 'data'
-    _cache_folder = _folder[:4] + '_cache' + _folder[4:]
-    _d_feather = os.path.join(_cache_folder, _filename+'.feather')
-    if not os.path.isdir(_cache_folder):
+    cache_folder = _folder[:4] + '_cache' + _folder[4:]
+    feather_filename = os.path.join(cache_folder, _filename+'.feather')
+    if not os.path.isdir(cache_folder):
         # Make cache folder
-        _print('  * Cache folder created:', _cache_folder)
-        os.mkdir(_cache_folder)
-    if os.path.isfile(_d_feather):
+        _print('  * Cache folder created:', cache_folder)
+        os.mkdir(cache_folder)
+    if os.path.isfile(feather_filename):
         # Read cache
-        _print('  * Reading cached file:', _d_feather)
-        _result = pd.read_feather(_d_feather)
+        _print('  * Reading cached file:', feather_filename)
+        result = pd.read_feather(feather_filename)
 
         t1 = time.time()
         _print(f'  * {(t1-t0)*1000:.3f} ms elapsed')
-        return _result
+        return result
     else:
         # Read csv, write cache, read cache
-        if _verbose:
-            print(colored('  * No cache found, reading csv file: ', 'red')+ colored(_d, 'blue'))
+        if verbose:
+            print(colored('  * No cache found, reading csv file: ', 'red')+ colored(filename, 'blue'))
         if _ext == '.tsv':
-            _result = pd.read_csv(_d, delimiter='\t')
+            result = pd.read_csv(filename, delimiter='\t')
         else:
-            _result = pd.read_csv(_d)
-        _shape = _result.shape
-        _print('  * Saving to cache:', _d_feather)
-        _result.to_feather(_d_feather)
-        _print('  * Reading cached file:', _d_feather)
-        _result = pd.read_feather(_d_feather)
-        assert _shape == _result.shape
+            result = pd.read_csv(filename)
+        expected_shape = result.shape
+        _print('  * Saving to cache:', feather_filename)
+        result.to_feather(feather_filename)
+        _print('  * Reading cached file:', feather_filename)
+        result = pd.read_feather(feather_filename)
+        assert result.shape == expected_shape
 
         t1 = time.time()
         _print(f'  * {(t1-t0)*1000:.3f} ms elapsed')
-        return _result
+        return result
 
-def cache_files(*_files):
-    for _f in _files:
-        read_cache(_f)
+def cache_files(*filenames):
+    for filename in filenames:
+        read_cache(filename)
 
-def cache_folder(_folder, _extensions=('.tsv', '.csv'), _recursive=True):
-    print(colored('Caching request for folder ', 'yellow')+colored(_folder, 'blue'))
+def cache_folder(folder, extensions=('.tsv', '.csv'), recursive=True):
+    print(colored('Caching request for folder ', 'yellow')+colored(folder, 'blue'))
     _t0 = time.time()
     _n_caches = 0
-    for _f in os.listdir(_folder):
-        _full_f = os.path.join(_folder, _f)
-        if os.path.isfile(_full_f) and os.path.splitext(_full_f)[1] in _extensions:
-            read_cache(_full_f, _verbose=True)
+    for _f in os.listdir(folder):
+        _full_f = os.path.join(folder, _f)
+        if os.path.isfile(_full_f) and os.path.splitext(_full_f)[1] in extensions:
+            read_cache(_full_f, verbose=True)
             _n_caches += 1
-        elif os.path.isdir(_full_f) and _recursive:
-            _n_caches += cache_folder(_full_f, _recursive=_recursive)
+        elif os.path.isdir(_full_f) and recursive:
+            _n_caches += cache_folder(_full_f, recursive=recursive)
     _dt = time.time() - _t0
-    print(colored(f'Cached {_n_caches} files in ', 'yellow')+colored(_folder, 'blue')+colored(f'. {_dt*1000:.0f} ms elapsed.', 'yellow'))
+    print(colored(f'Cached {_n_caches} files in ', 'yellow')+colored(folder, 'blue')+colored(f'. {_dt*1000:.0f} ms elapsed.', 'yellow'))
     return _n_caches
 
 def read(filename):
